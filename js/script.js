024f2d78e172cba2cf5b29cf26ef458d76eae7ae
danydifilippo <danydifilippo@gmail.com>
2022-10-30 10:27:38 +0100

    to_complete

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 "use strict";
 class Smartphone {
     constructor() {
-        this.carica = 0;
         this.numeroChiamate = 0;
         this.oreChiamate = 0;
         this.minutiChiamate = 0;
@@ -37,12 +36,6 @@ class Smartphone {
         }
         ;
         this.numeroChiamate++;
-    }
-    numero404() {
-        this.credito;
-    }
-    getNumeroChiamate() {
-        this.numeroChiamate;
     }
     azzeraChiamate() {
         this.numeroChiamate = 0;
@@ -101,7 +94,7 @@ class Utente extends User {
 }
 let user1 = new Utente("Giulia", 22, iphone, "amiche e ragazzo", "dai 20 ai 40 minuti", "../img/giulia.jpeg", "utente1", "studentessa");
 let user2 = new Utente("Maria", 52, huawei, "familiari vicini e lontani", "dai 30 ai 40 minuti", "../img/maria.jpg", "utente2", "casalinga");
-let user3 = new Utente("Pietro", 41, samsung, "fornitori e trasportatori", "dai 15 ai 20 minuti", "../img/pietro.jpeg", "utente3", "Concessionario ");
+let user3 = new Utente("Pietro", 41, samsung, "fornitori e trasportatori", "dai 15 ai 20 minuti", "../img/pietro.jpeg", "utente3", "Rivenditore di auto");
 let users = [user1, user2, user3];
 window.onload = () => {
     let date = new Date();
@@ -123,7 +116,81 @@ window.onload = () => {
           <h3>${element.name}</h3>
           <img src=${element.picture} alt="foto_user" />
         </div>
-        </div>
-    </section>`;
+        <div class="buttons">
+        <p><button class="creditobtn btn"><i class="fas fa-euro-sign"></i></button>Credito</p>
+        <p><button class="chiamabtn btn"><i class="fas fa-phone"></i></button>Chiama</p>
+        <p><button class="ricaricabtn btn"><i class="fas fa-sync-alt"></i></button>Ricarica</p>
+        <div class="d-none credit">
+        <h3><strong>Credito disponibile</strong></h3>
+    <p>Il tuo credito residuo è di ${element.smartphone.credito} €</p>
+    <input type="button" value="OK" class="close"><input type="button" value="Ricarica" class="charge"></div>
+      </div>
+  <div class="d-none ricarica">
+<h3><strong>Ricarica il tuo credito</strong></h3>
+<p>Inserisci l'importo e conferma</p>
+<input type="number" class="chargeInput">
+<input type="button" value="Ricarica" class="ricarbtns ricbtn"><input type="button" value="Esci" class="close ricarbtns"></div>      
+</div>
+    </section>
+    <div class="d-none chiamare">
+
+    </div>`;
+    });
+    let credito = document.querySelectorAll('.creditobtn');
+    credito.forEach(utn => {
+        utn.addEventListener('click', () => {
+            var _a, _b, _c;
+            (_c = (_b = (_a = utn.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.lastElementChild) === null || _c === void 0 ? void 0 : _c.classList.remove('d-none');
+        });
+    });
+    let ricarica = document.querySelectorAll('.ricaricabtn');
+    ricarica.forEach(ute => {
+        ute.addEventListener('click', () => {
+            var _a, _b, _c, _d;
+            (_d = (_c = (_b = (_a = ute.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.parentNode) === null || _c === void 0 ? void 0 : _c.lastElementChild) === null || _d === void 0 ? void 0 : _d.classList.remove('d-none');
+        });
+    });
+    let close = document.querySelectorAll('.close');
+    close.forEach(el => {
+        el.addEventListener('click', () => {
+            var _a, _b, _c;
+            (_c = (_b = (_a = el.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.lastElementChild) === null || _c === void 0 ? void 0 : _c.classList.add('d-none');
+        });
+        let charge = document.querySelectorAll('.charge');
+        charge.forEach(ele => {
+            ele.addEventListener('click', () => {
+                var _a, _b, _c, _d, _e, _f, _g;
+                (_d = (_c = (_b = (_a = ele.parentNode) === null || _a === void 0 ? void 0 : _a.parentNode) === null || _b === void 0 ? void 0 : _b.parentNode) === null || _c === void 0 ? void 0 : _c.lastElementChild) === null || _d === void 0 ? void 0 : _d.classList.remove('d-none');
+                (_g = (_f = (_e = ele.parentNode) === null || _e === void 0 ? void 0 : _e.parentNode) === null || _f === void 0 ? void 0 : _f.lastElementChild) === null || _g === void 0 ? void 0 : _g.classList.add('d-none');
+            });
+        });
+        let ricbtn = document.querySelectorAll('.ricbtn');
+        ricbtn.forEach(elem => {
+            elem.addEventListener('click', () => {
+                // elem.parentNode?.parentNode?.lastElementChild?.classList.add('d-none')
+                let chargeInput = document.querySelectorAll('.chargeInput');
+                console.log(chargeInput.values());
+            });
+        });
+        //     let charge1 = document.querySelector('#charge_utente1') as HTMLButtonElement
+        //         charge1.addEventListener('click',()=>{
+        //             noteCredit1.classList.add('d-none')
+        //             let ricarica1 = document.querySelector('#utente1_ricarica') as HTMLButtonElement
+        //             ricarica1.classList.remove('d-none')
+        //     })
+        // })
+        // })
+        // let ricarica = document.querySelector('#Giulia_Char') as HTMLInputElement
+        // ricarica.addEventListener ('click', ()=>{
+        //     let noteRicarica = document.querySelector('#Giulia_ricarica') as HTMLDivElement
+        //     console.log(ricarica.children.namedItem);
+        //      ricarica.childNodes
+        // })
+        // let chiama = document.querySelectorAll<HTMLDivElement>('.chiama')
+        // chiama.forEach(utn => {
+        //     utn.addEventListener('click', () => {
+        //        console.log(utn.parentNode?.parentNode?.parentNode?.lastElementChild);
+        //        utn.nextElementSibling
+        //     } )
     });
 };
--- ts/script.ts
+++ ts/script.ts
@@ -1,5 +1,4 @@
 class Smartphone {
-    carica:number=0
     numeroChiamate:number=0
     oreChiamate:number=0
     minutiChiamate:number=0
@@ -32,13 +31,6 @@ class Smartphone {
         this.numeroChiamate ++
     }
 
-    numero404(){
-        this.credito
-    }
-
-    getNumeroChiamate(){
-        this.numeroChiamate
-    }
 
     azzeraChiamate(){
         this.numeroChiamate = 0
@@ -119,7 +111,7 @@ class Utente extends User {
 
 let user1 = new Utente ("Giulia", 22, iphone,"amiche e ragazzo","dai 20 ai 40 minuti","../img/giulia.jpeg","utente1","studentessa")
 let user2 = new Utente ("Maria", 52, huawei,"familiari vicini e lontani","dai 30 ai 40 minuti","../img/maria.jpg","utente2","casalinga")
-let user3 = new Utente ("Pietro", 41, samsung,"fornitori e trasportatori","dai 15 ai 20 minuti","../img/pietro.jpeg","utente3","Concessionario ")
+let user3 = new Utente ("Pietro", 41, samsung,"fornitori e trasportatori","dai 15 ai 20 minuti","../img/pietro.jpeg","utente3","Rivenditore di auto")
  
 let users = [user1, user2, user3]
 
@@ -143,7 +135,83 @@ users.forEach(element => {
           <h3>${element.name}</h3>
           <img src=${element.picture} alt="foto_user" />
         </div>
-        </div>
-    </section>`});
-    }
+        <div class="buttons">
+        <p><button class="creditobtn btn"><i class="fas fa-euro-sign"></i></button>Credito</p>
+        <p><button class="chiamabtn btn"><i class="fas fa-phone"></i></button>Chiama</p>
+        <p><button class="ricaricabtn btn"><i class="fas fa-sync-alt"></i></button>Ricarica</p>
+        <div class="d-none credit">
+        <h3><strong>Credito disponibile</strong></h3>
+    <p>Il tuo credito residuo è di ${element.smartphone.credito} €</p>
+    <input type="button" value="OK" class="close"><input type="button" value="Ricarica" class="charge"></div>
+      </div>
+  <div class="d-none ricarica">
+<h3><strong>Ricarica il tuo credito</strong></h3>
+<p>Inserisci l'importo e conferma</p>
+<input type="number" class="chargeInput">
+<input type="button" value="Ricarica" class="ricarbtns ricbtn"><input type="button" value="Esci" class="close ricarbtns"></div>      
+</div>
+    </section>
+    <div class="d-none chiamare">
+
+    </div>`});
+    let credito = document.querySelectorAll<HTMLDivElement>('.creditobtn')
+    credito.forEach(utn => {
+        utn.addEventListener('click', () => {
+        utn.parentNode?.parentNode?.lastElementChild?.classList.remove('d-none')
+        })
+    })
+    let ricarica = document.querySelectorAll<HTMLDivElement>('.ricaricabtn')
+    ricarica.forEach(ute => {
+        ute.addEventListener('click', () => {
+        ute.parentNode?.parentNode?.parentNode?.lastElementChild?.classList.remove('d-none')
+        })
+    })
+    let close = document.querySelectorAll<HTMLDivElement>('.close')
+    close.forEach(el=> {
+        el.addEventListener('click', () => {
+        el.parentNode?.parentNode?.lastElementChild?.classList.add('d-none')
+    })
+    let charge = document.querySelectorAll<HTMLDivElement>('.charge')
+    charge.forEach(ele=> {
+        ele.addEventListener('click', () => {
+        ele.parentNode?.parentNode?.parentNode?.lastElementChild?.classList.remove('d-none')
+        ele.parentNode?.parentNode?.lastElementChild?.classList.add('d-none')
+        })
+    })
+    let ricbtn = document.querySelectorAll<HTMLDivElement>('.ricbtn')
+    ricbtn.forEach(elem=> {
+        elem.addEventListener('click', () => {
+        // elem.parentNode?.parentNode?.lastElementChild?.classList.add('d-none')
+         let chargeInput = document.querySelectorAll<HTMLDivElement>('.chargeInput')
+           console.log(chargeInput.values());
+            
+        
+        })
+    })
+   
+    //     let charge1 = document.querySelector('#charge_utente1') as HTMLButtonElement
+    //         charge1.addEventListener('click',()=>{
+    //             noteCredit1.classList.add('d-none')
+    //             let ricarica1 = document.querySelector('#utente1_ricarica') as HTMLButtonElement
+    //             ricarica1.classList.remove('d-none')
+    //     })
+    // })
+    // })
+
+    // let ricarica = document.querySelector('#Giulia_Char') as HTMLInputElement
+    // ricarica.addEventListener ('click', ()=>{
+    //     let noteRicarica = document.querySelector('#Giulia_ricarica') as HTMLDivElement
+    //     console.log(ricarica.children.namedItem);
+    //      ricarica.childNodes
+    // })
+    // let chiama = document.querySelectorAll<HTMLDivElement>('.chiama')
+    // chiama.forEach(utn => {
+    //     utn.addEventListener('click', () => {
+    //        console.log(utn.parentNode?.parentNode?.parentNode?.lastElementChild);
+    //        utn.nextElementSibling
+    //     } )
+        
+     });
+}
+    
     
